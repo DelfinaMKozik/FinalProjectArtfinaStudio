5fe7a12178e37eaa79ce0ce0390bd2156bcef09d
DelfinaMKozik <delfina.kozik@gmail.com>
2023-09-09 22:12:23 +0100

    added routes, links, downloaded font awesome icons

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import "./app.css";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 // import Footer from "./components/Footer";
-// import Shop from "./pages/Shop";
-// import About from "./pages/About";
-// import Contact from "./pages/Contact";
+import Shop from "./pages/Shop";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Gallery from "./pages/Gallery"
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
 
 
@@ -17,7 +18,11 @@ function App() {
             <div>hehehe</div>
             <Router>
                 <Routes>
-                    <Route path="/home" exact component={Home}/>
+                    <Route path="/home" element={<Home/>}/>
+                    <Route path="/about" element={<About/>}/>
+                    <Route path="/gallery" element={<Gallery/>}/>
+                    <Route path="/shop" exact component={<Shop/>}/>
+
                 </Routes>
             </Router>
             <Navbar/>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,29 @@
 import React from "react";
+import {Link} from "react-router-dom"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { faUser, faBagShopping } from '@fortawesome/free-solid-svg-icons'
+
 
 export default function Navbar() {
     return (
         <div className="navBar">
+            <ul className="menu">
+                <li>
+                    <Link to="/about">ABOUT</Link>
+                    <Link to="/gallery">GALLERY</Link>
+                    <Link to="/shop">SHOP</Link>
+                </li>
+            </ul>
+            <h1 className="logo">ArtFinaStudio</h1>
+            <div className="action-outer">
+                <div className="user">
+                    <FontAwesomeIcon icon={faUser} style={{color: "#000000",}} />
+
+                </div>
+                <div className="card">
+                        <FontAwesomeIcon icon={faBagShopping} style={{color: "#000000",}} />
+                </div>
+            </div>
         </div>
     )
 }
