278581027626fdc7af08583394fe016bae8a52ce
DelfinaMKozik <delfina.kozik@gmail.com>
2023-09-09 15:05:46 +0100

    correct repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./app.css";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+// import Footer from "./components/Footer";
+// import Shop from "./pages/Shop";
+// import About from "./pages/About";
+// import Contact from "./pages/Contact";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div>hehehe</div>
+            <Router>
+                <Routes>
+                    <Route path="/home" exact component={Home}/>
+                </Routes>
+            </Router>
+            <Navbar/>
+
+        </div>
+    );
 }
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div>Hello you</div>
+    );
+}
+
+export default Home;
