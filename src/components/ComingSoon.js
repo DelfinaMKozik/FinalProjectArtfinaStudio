efba20a50e54f3c6c9b92e421493a6265b60b99e
DelfinaMKozik <delfina.kozik@gmail.com>
2023-09-16 12:25:40 +0100

    updated colors

--- src/components/ComingSoon.js
+++ src/components/ComingSoon.js
@@ -2,7 +2,7 @@ import React from 'react';
 import comingSoon from "../assets/ComingSoon.jpg"
 function ComingSoon(props) {
     return (
-        <img src={comingSoon} alt="Logo" />
+        <img src={comingSoon} alt="website in progress, coming soon" />
     );
 }
 
